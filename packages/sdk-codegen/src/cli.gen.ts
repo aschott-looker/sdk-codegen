71b30f091c95b42f0c792274c487d69f5beb1c5e
Anthony Schott <anthonyschott@google.com>
2022-03-03 03:19:35 +0000

    Remove dead code

--- packages/sdk-codegen/src/cli.gen.ts
+++ packages/sdk-codegen/src/cli.gen.ts
@@ -272,18 +272,6 @@ ${addCommandsCode}}
     return str.replace(new RegExp(find, 'g'), replace)
   }
 
-  // capitalize(str: string): string {
-  //   return str.charAt(0).toUpperCase() + str.slice(1)
-  // }
-
-  // toCamelCaseCap(str: string): string {
-  //   return this.capitalize(
-  //     str.replace(/([-_][a-z])/g, (group) =>
-  //       group.toUpperCase().replace('-', '').replace('_', '')
-  //     )
-  //   )
-  // }
-
   toCamelCase(str: string): string {
     return str.replace(/([-_][a-z])/g, (group) =>
       group.toUpperCase().replace('-', '').replace('_', '')
